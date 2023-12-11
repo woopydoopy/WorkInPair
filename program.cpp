7652bb09c7d1f1559e820437a6cfe5994933f030
Anna Seletskaya <seletskaya.anna.stu@mail.ru>
2023-12-11 04:25:22 +0300

    Snapshot2, program.cpp, funcs.h, funcs.cpp:

--- program.cpp
+++ program.cpp
@@ -1,14 +1,7 @@
 //файл program.cpp
 #include <iostream>
+#include "funcs.h"
 using namespace std;
-double MySum(double a, double b)
-{
-return a+b;
-}
-double MySub(double a, double b)
-{
-return a - b;
-}
 int main()
 {
 cout << MySum(10, 3) << endl;
